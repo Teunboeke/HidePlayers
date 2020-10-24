4bc94bf0bde1f83a5f00d2c79fd5c7bbd0d5465d
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-24 13:15:49 +0200

    Update Hide.php

--- src/Teunboeke/Hideplayers/Hide.php
+++ src/Teunboeke/Hideplayers/Hide.php
@@ -1,3 +1,11 @@
 <?php
 
 namespace Teunboeke/hideplayers;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+class hideplayers extends PluginBase implements Listener{
