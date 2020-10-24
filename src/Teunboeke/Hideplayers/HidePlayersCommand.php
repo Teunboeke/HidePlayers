b2349f86b380ed61c84ad1b5136b9827afbb0400
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-24 13:32:15 +0200

    Update HidePlayersCommand.php

--- src/Teunboeke/Hideplayers/HidePlayersCommand.php
+++ src/Teunboeke/Hideplayers/HidePlayersCommand.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Teunboeke/hideplayers 
+  
+  use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
