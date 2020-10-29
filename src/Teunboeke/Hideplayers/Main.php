1786beea60d062af483e759ad8d0687a2f52a581
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-29 18:03:03 +0100

    Update Main.php

--- src/Teunboeke/Hideplayers/Main.php
+++ src/Teunboeke/Hideplayers/Main.php
@@ -12,8 +12,8 @@ use pocketmine\utils\Config;
 class Main extends PluginBase implements Listener{
   
   	public function onEnable() {
-              $this->getServer()->getCommandMap()->register("hideplayers", new HidePlayerCommand($this));
-            $this->getServer()->getCommandMap()->register("showplayers", new ShowPlayersCommand($this));
+              $this->getServer()->getCommandMap()->register("hideplayers", new HidePlayersCommand($this));
+            $this->getServer()->getCommandMap()->register("showplayers", new ShowPlayerCommand($this));
       		$this->saveResource("settings.yml");
   		$settings = new Config($this->getDataFolder() . "settings.yml", Config::YAML); 
           }
