388fd6d179dc36f337ac4a770e1bacf5ac2b3ff8
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-30 16:57:19 +0100

    Update HidePlayersCommand.php

--- src/Teunboeke/Hideplayers/HidePlayersCommand.php
+++ src/Teunboeke/Hideplayers/HidePlayersCommand.php
@@ -32,7 +32,7 @@ class HidePlayersCommand extends Command {
       		foreach(Server::getInstance()->getOnlinePlayers() as $players){
                         $sender->hidePlayer($players);
                     }
-              $this->file = new Config($this->getDataFolder() . "settings.yml", Config::YAML);
+              $this->getplugin new Config($this->getDataFolder() . "settings.yml", Config::YAML);
              $sender->sendMesssage($this->file->get("hide-player-message"));
               return false;
           }
